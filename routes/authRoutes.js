6ed444a3b7c62d2d13ba07e841ba3175773feb54
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-10-27 13:01:42 +0530

    added logout route for the user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,6 +7,14 @@ module.exports = (app) => {
 
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  app.get('/api/logout', (req, res) => {
+  	// Passport automatically attaches user to the req object with some other funtions.
+  	// logout is also attached and can be used to expire the cookie.
+  	req.logout();
+  	// req.user is destroyed by passport, we should see a empty screen
+  	res.send(req.user);
+  })
+
   app.get('/api/current_user', (req, res) => {
   	res.send(req.user);
   });
