12a733cf1a28a9c8f5dbe37c92ddfe57013c5243
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-10-29 20:30:47 +0530

    added redux store in index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
