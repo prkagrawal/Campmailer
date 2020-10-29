7fcb13423f408a59877e2db2a7d0c4cfdc1db410
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-10-29 19:53:46 +0530

    passport service refactor

--- services/passport.js
+++ services/passport.js
@@ -29,19 +29,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if (existingUser) {
-            // we already have a record with the given profile id.
-            done(null, existingUser);
-          } else {
-            // we don't have a user record wih this id, make a new record.
-            new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user))
-          }
-        })
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
