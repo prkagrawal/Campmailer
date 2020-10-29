f22a5db1b0f9e39b4c9fb884683b0fca66704e78
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-10-29 20:57:25 +0530

    react router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './App.css';
+
+const Header = () => {
+  return (
+    <h2>Header</h2>
+  );
+}
+
+const Dashboard = () => {
+  return (
+    <h2>Dasboard</h2>
+  );
+}
+
+const SurveyNew = () => {
+  return (
+    <h2>SurveyNew</h2>
+  );
+}
+
+const Landing = () => {
+  return (
+    <h2>Landing</h2>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <h1> Hi </h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={Landing} />
+      </div>
+    </BrowserRouter>
   );
 }
 
