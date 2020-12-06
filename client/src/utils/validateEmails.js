fa276ab1b17ea69249d96f80a772375d691e5726
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-12-06 21:55:29 +0530

    added email validator

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable import/no-anonymous-default-export */
 // Regular expression to validate email.
 const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
